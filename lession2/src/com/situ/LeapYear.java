1ff4aab9c039b68f21598f854689f30376e078f4
lishuo <1344793627@qq.com>
2020-09-22 17:08:19 +0800

    判断某年是不是闰月完成

--- lession2/src/com/situ/LeapYear.java
+++ lession2/src/com/situ/LeapYear.java
@@ -10,25 +10,43 @@ public class LeapYear {
 		try (Scanner sc = new Scanner(System.in)) {
 			year = sc.nextInt();
 		}
-		if(year>0)
-		{
-			if ((0 == year % 4) && (0 != year % 100) && (0 == year % 400)) {
-				System.out.println(year+"为闰年");
+		if (year > 0) {
+			if (((0 == year % 4) && (0 != year % 100)) || (0 == year % 400)) {
+				System.out.print(year + "为闰年");
+				if (0 == year % 400) {
+					System.out.println(",且为世纪闰年");
+				} else {
+
+					System.out.println(",且为普通闰年");
+				}
 			} else {
+				System.out.println("不是闰年");
 				if (0 != year % 4) {
-					System.out.println(year+"不能被4整除，所以不是闰年");
+					System.out.println(year + "不能被4整除");
 				}
 				if (0 == year % 100) {
-					System.out.println(year+"能被100整除，所以不是闰年");
+					System.out.println(year + "能被100整除");
 				}
 				if (0 != year % 400) {
-					System.out.println(year+"不能被400整除，所以不是闰年");
+					System.out.println(year + "不能被400整除");
 				}
 			}
-		}else
-		{
+		} else {
 			System.out.println("请输入正确的年份");
-		}
+		}   
+//		if (year % 4 == 0) {
+//            if (year % 100 != 0) {
+//                System.out.println("闰年");
+//            } else {
+//                if (year % 400 == 0) {
+//                    System.out.println("闰年");
+//                } else {
+//                    System.out.println("平年");
+//                }
+//            }
+//        } else {
+//            System.out.println("平年");
+//        }
 	}
 
 }
