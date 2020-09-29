8759a2e9a4821d823ae1ae0f4de1a52b5a3cd0d4
lishuo <1344793627@qq.com>
2020-09-29 17:02:56 +0800

    猜数

--- gassnum/src/com/situ/GuessNum.java
+++ gassnum/src/com/situ/GuessNum.java
@@ -6,14 +6,11 @@ import java.util.Scanner;
 public class GuessNum {
 
 	public static void main(String[] args) {
-		int numGuessMax, numGuessMin;
+		int numGuessMax = 100, numGuessMin = 0;
 		int num = new Random().nextInt(100);
 		System.out.println("请输入0-100进行猜数");
 		Scanner sc = new Scanner(System.in);
-		Scanner sc1 = new Scanner(System.in);
 		int numGuess = sc.nextInt();
-		numGuessMax = 100;
-		numGuessMin = 0;
 		while (true) {
 			if (numGuess > num) {
 				System.out.println("这个数大于正确答案");
@@ -28,13 +25,12 @@ public class GuessNum {
 				}
 				System.out.println("要猜的数处于" + numGuessMin + "与" + numGuessMax + "之间");
 			}
-			numGuess = sc1.nextInt();
+			numGuess = sc.nextInt();
 			if (numGuess == num) {
 				System.out.println("这就是正确答案，正确答案为：" + num);
 				break;
 			}
 		}
-		sc1.close();
 		sc.close();
 	}
 }
